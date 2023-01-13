d3631e07a2bd863573c02f85e93a32de0fd9ea8b
muturich <calvinrich20@gmail.com>
2023-01-13 13:29:56 -0600

    update server.js

--- server.js
+++ server.js
@@ -53,6 +53,91 @@ function firstPrompt(){
             "END"]
 
     })
+.then (function({ task}){
+    switch (task) {
+        case "View Employees":
+         viewEmployee();
+         break;
 
-    
+        case "View Employees by Department":
+         viewEmployeeByDepartment();
+         break;
+        
+        case "Add Employee":
+         addEmployee();
+         break;
+
+        case "Remove Employees":
+         removeEmployees();
+         break;
+
+        case "Update Employee Role":
+         updateEmployeeRole();
+         break;
+        case "Add Role":
+         addRole();
+         break;
+        case "End":
+         connection.end();
+         break;
+    }
+});
+
 }
+
+//view Employees/Read all. SELECT *FROM
+ 
+function viewEmployee (){
+    console.log("Viewing employees\n");
+
+    var query = 
+    `SELECT e.id, e.first_name, e.last_name,r.title,d.name AS department,r.salary,CONCAT(m.first_name, '',m.last_name)AS manager
+    FROM employee e
+    LEFT JOIN role r
+         ON e.role_id = r.id
+    LEFT JOIN department d
+    on d.id = r.department_id
+    LEFT Join employee m 
+        ON m.id = e.manager_id`
+
+    connection.query(query, function (err, res){
+        if (err) throw err;
+
+        console.table(res);
+        console.log("Employees viewed!\n");
+
+        firstPrompt();
+});
+}
+
+
+//"view Employees by Department" / READ by, SELECT * FROM 
+//Make a department array
+
+function viewEmployeeByDepartment() {
+    console.log("Viewing employees by department\n");
+
+    var query = 
+    `SELECT d.id, d.name, r.salary AS budget
+    FROM employee e
+    LEFT JOIN role r
+          ON e.role_id = r.id
+    LEFT JOIN department d
+    ON d.id = r.department_id
+    GROUP BY d.id, d.name `
+
+    connection.query(query, function(err, res) {
+     if (err) throw err;
+
+     const departmentChoices = res.map(data => ({
+        value: data.id, name: data.name
+     }));
+     console.table(res);
+     console.log("Department view succeed!\n");
+
+     promptDepartment(departmentChoices);
+     
+    });
+
+}
+
